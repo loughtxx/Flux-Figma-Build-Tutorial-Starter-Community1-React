440bade09bdf586ba253c18429557f9b50d5ef5c
loughtxx <111271622+loughtxx@users.noreply.github.com>
2023-01-08 02:48:56 +0700

    Commit from TeleportHQ

---
+++ src/views/desktop1.js
@@ -0,0 +1,62 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './desktop1.css'
+
+const Desktop1 = (props) => {
+  return (
+    <div className="desktop1-container">
+      <Helmet>
+        <title>exported project</title>
+      </Helmet>
+      <div className="desktop1-desktop1">
+        <img
+          alt="Rectangle252056"
+          src="/playground_assets/rectangle252056-0jb-400w.png"
+          className="desktop1-rectangle25"
+        />
+        <img
+          alt="Rectangle262057"
+          src="/playground_assets/rectangle262057-26ma-400w.png"
+          className="desktop1-rectangle26"
+        />
+        <span className="desktop1-text">
+          <span>
+            <span>Euphoria’s</span>
+            <br></br>
+            <span>Gallery.</span>
+          </span>
+        </span>
+        <span className="desktop1-text5">
+          <span>Welcome To My Fuckin’ Gallery</span>
+        </span>
+        <span className="desktop1-text7">
+          <span>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu
+            purus purus. Vestibulum viverra, diam eu ullamcorper semper, augue
+            felis maximus ipsum, in mattis eros orci eu dolor. Aliquam dignissim
+            non velit in vehicula. Suspendisse potenti. Nulla id scelerisque
+            diam. Morbi efficitur turpis nec ornare vehicula. Suspendisse
+            suscipit ex et tellus scelerisque aliquam. Pellentesque tellus
+            libero, scelerisque non tincidunt id, luctus et erat. In mollis diam
+            sed massa porttitor, at malesuada nisi lacinia. Orci varius natoque
+            penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+            posuere cubilia curae; Integer ligula elit, facilisis ac mauris nec,
+            mattis pellentesque lectus. Aenean rutrum lorem et ante dapibus, et
+            eleifend neque molestie. Nullam luctus malesuada mauris, pulvinar
+            tincidunt sapien.
+          </span>
+        </span>
+        <img
+          alt="Rectangle242055"
+          src="/playground_assets/rectangle242055-qmzk-400w.png"
+          className="desktop1-rectangle24"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Desktop1
